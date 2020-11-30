38e182ff3281b094ff3802d122bdb7b653588ec5
limunosekai <powerlsh0103@gmail.com>
2020-11-30 22:21:15 +0900

    First

---
+++ main.js
@@ -0,0 +1,13 @@
+let age = 100;
+let age2 = age;
+
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
+
+let name = 'lim';
+let name2 = name;
+
+console.log(name, name2);
+name = 'kim';
+console.log(name, name2);
